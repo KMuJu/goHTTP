9220aedda395b186de493519159477e9f819cc82
Kasper <kasper.mustad.juell@linux.com>
2024-08-06 21:59:07 +0200

    Add string method to Request

--- internal/http.go
+++ internal/http.go
@@ -1,5 +1,7 @@
 package internal
 
+import "strings"
+
 type method string
 
 type message struct {
@@ -18,4 +20,22 @@ type Request struct {
 
 type RequestWriter struct {
 	bytes []byte
+}
+
+func (r Request) String() string {
+	builder := strings.Builder{}
+	builder.WriteString(r.Method)
+	builder.WriteString(" ")
+	builder.WriteString(r.URL)
+	builder.WriteString(" ")
+	builder.WriteString(r.Version)
+	builder.WriteString("\r\n")
+	for k, v := range r.Header {
+		builder.WriteString(k + ":")
+		builder.WriteString(strings.Join(v, ", "))
+		builder.WriteString("\r\n")
+	}
+	builder.WriteString("\r\n")
+	builder.Write(r.Body)
+	return builder.String()
 }
