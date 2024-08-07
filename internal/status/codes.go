4ef5cb6bb48ca95f3b9adef9f03c14ad21b92a40
Kasper <kasper.mustad.juell@linux.com>
2024-08-07 15:38:44 +0200

    Add more Error codes

--- internal/status/codes.go
+++ internal/status/codes.go
@@ -3,7 +3,8 @@ package status
 type Code int
 
 var (
-	Ok       Code = 200
-	NotFound Code = 400
-	Found    Code = 302
+	Ok                  Code = 200
+	NotFound            Code = 400
+	Found               Code = 302
+	InternalServerError Code = 500
 )
